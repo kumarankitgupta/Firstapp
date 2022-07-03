202b904ec05751be9489377861105bc243d47288
Ankit Kumar Gupta <ankitkrgupta149@gmail.com>
2022-07-04 02:21:39 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, SafeAreaView ,Image,TouchableHighlight,Button,Alert,Platform} from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{color:"green"}}>Application</Text>
+      <Text style={{fontSize:20,color:"red"}}>Achiever First App</Text>
+      <TouchableHighlight onPress={() =>console.log("Image Tapped")}>
+      <Image
+       style={styles.img} source={require("./assets/logo.gif")} />
+      </TouchableHighlight>
+      <Button title="click me" onPress={() => Alert.alert("Reviews","Do you Like My App",
+      [{text:"yes",onPress: () => console.log("Yes")},
+      {text:"no",onPress: () => console.log("No")}])} />
+      <Text style={{color:"red",paddingTop:20}}>Created on date : 3rd July 2022</Text>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#CCF3EE",
+    alignItems:'center',
+    justifyContent:'center',
+    paddingTop:Platform.OS === "android" ? 40:0
   },
+  img:{
+    width:200,
+    height:200,
+    borderRadius:40,
+    marginBottom:10,
+  }
 });
